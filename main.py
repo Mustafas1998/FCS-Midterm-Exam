1fff59d6ba617f40919409cc3c38060633ddf8e2
Mustafas1998 <ms6534126@gmail.com>
2023-08-20 12:03:58 +0300

    Change Four

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+from collections import Counter
 def transferring_data(directory):
     """This function is to transfer data from file to dictionaries"""
     #https://www.geeksforgeeks.org/python-datetime-strptime-function/ used this function to format time
@@ -12,7 +13,7 @@ def transferring_data(directory):
                 'username': username,
                 'employee_ID': employee_ID,
                 'salary': int(salary),
-                'Gender': Gender,
+                'Gender': Gender
             })
     return data_of_employees
 
@@ -34,18 +35,31 @@ def Menu_admin():
     print("5. Remove an Employee")
     print("6. Raise an Employee's Salary")
     print("7. Exit")
-    
+
 def Menu_Employee():
     print("\nEmployee Menu:")
     print("1. Check Salary")
     print("2. Exit")
 
-
+def adding_new_employee(data_of_employees):
+    # https://www.toppr.com/guides/python-guide/references/methods-and-functions/methods/string/zfill/python-string-zfill/
+    # using the rjust() to format new employee id
+    username = input("Enter username of new employee: ")
+    new_ID = f"emp{str(len(data_of_employees) + 1).rjust(3, '0')}"
+    salary = int(input("Enter the salary: "))
+    Gender = input("Enter gender, (male/female): ")
+    data_of_employees.append({
+        'date_joined': datetime.datetime.now(), # now for formatting joining timegit 
+        'username': username,
+        'employee_ID': new_ID,
+        'salary': salary,
+        'Gender': Gender
+    })
+    print("New employee was added!")
 def Main_program():
     data_of_employees = transferring_data("employees.txt")
     numb_of_attempts = 0
     numb_of_max_attempts = 5
-
     while numb_of_attempts < numb_of_max_attempts:
         username = input("Enter username: ")
         password = input("Enter password: ")
@@ -57,7 +71,7 @@ def Main_program():
                 if choice == '1':
                     print('1')
                 elif choice == '2':
-                    print('2')
+                    adding_new_employee(data_of_employees)
                 elif choice == '3':
                     print('3')
                 elif choice == '4':
@@ -72,7 +86,7 @@ def Main_program():
                     break
                 else:
                     print("Please select a valid option!")
-        elif username != 'admin' or password != 'admin123123':
+        elif username == 'admin' and password != 'admin123123':
             print("Invalid password and/or username")
 
         is_employee = False
@@ -103,9 +117,9 @@ def Main_program():
                     break
         if not is_employee and (username != "admin" or password != 'admin123123'):
             numb_of_attempts += 1
-            # print("Invalid username and/or password")
         if numb_of_attempts >= numb_of_max_attempts:
             print("You have reached maximum number of login attempts. Goodbye.")
+
 Main_program()
 
 
